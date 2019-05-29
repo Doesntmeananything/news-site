e2acf84b72ec90ded847164af1505d2fe82e174e
Doesntmeananything <andrey.krasnov.pm@gmail.com>
2019-05-29 16:50:21 +0300

    Add Base Web library

--- README.md
+++ README.md
@@ -2,6 +2,7 @@
 
 A single page app built with:
 
-- React and React hooks;
-- Create-react-app;
+- React and React hooks
+- [Base Web React Components](https://baseweb.design/)
+- Create-react-app
 - Powered by [NewsAPI.org](https://newsapi.org)
--- src/index.js
+++ src/index.js
@@ -1,10 +1,19 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import { Provider as StyletronProvider } from "styletron-react";
+import { Client as Styletron } from "styletron-engine-atomic";
+import "./index.css";
+import App from "./App";
+import * as serviceWorker from "./serviceWorker";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const engine = new Styletron();
+
+ReactDOM.render(
+  <StyletronProvider value={engine}>
+    <App />
+  </StyletronProvider>,
+  document.getElementById("root")
+);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
