7a9cde8fda0556cdaf4e6e88f8889c9d1361c7ee
Doesntmeananything <andrey.krasnov.pm@gmail.com>
2019-06-03 20:54:51 +0300

    Add footer contents

---
+++ src/components/FooterContent.js
@@ -0,0 +1,22 @@
+import React from "react";
+import { Label1 } from "baseui/typography";
+import { StyledLink } from "baseui/link";
+
+export default () => (
+  <Label1
+    backgroundColor="mono900"
+    color="mono400"
+    display="flex"
+    justifyContent="center"
+    alignItems="center"
+    style={{ width: "100%", height: "70px", marginTop: "12px" }}
+  >
+    Powered by&nbsp;
+    <StyledLink
+      style={{ color: "#E5E5E5", textDecoration: "underline" }}
+      href="https://newsapi.org/"
+    >
+      News API
+    </StyledLink>
+  </Label1>
+);
